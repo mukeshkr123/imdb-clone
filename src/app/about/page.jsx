c887e895c0dee24e8a72a626efaebd0db8856fc3
Mukesh Kumar <mkmehta2041@gmail.com>
2023-08-29 22:28:06 +0530

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who are dedicated to creating high-quality content about
+        movies.
+      </p>
+      <p>
+        Our website features a wide variety of content, including movie reviews,
+        news articles, lists, and a database of movies that you can browse by
+        genre, year, or rating. We are always looking for new and exciting ways
+        to share our love of movies with the world.
+      </p>
+      <p>
+        If you have any ideas for content that you would like to see on our
+        website, please let us know. We would love to hear from you! Here are a
+        few examples of the content that you can find on our website: Movie
+        reviews: We write in-depth reviews of the latest movies, both
+        blockbusters and independent films.
+      </p>
+      <p>
+        News articles: We keep you up-to-date on the latest news in the movie
+        industry, from casting announcements to box office results. Lists: We
+        create lists of the best movies of all time, as well as lists of movies
+        based on genre, actor, or director.
+      </p>
+      <p>
+        Database: Our database of movies includes information on over 100,000
+        films, including the title, release date, genre, rating, and cast. We
+        hope you enjoy our website and find the content that you are looking
+        for.
+      </p>
+      <p>
+        If you have any questions or feedback, please do not hesitate to contact
+        us.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <div className="flex  justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
-        <MenuItem title="ABOUT" address="/" Icon={BsInfoCircle} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircle} />
       </div>
       <div className="flex items-center space-x-4">
         <DarkModeSwitch />
