de49f10375513953386429021a28fa8505ca4e15
Mukesh Kumar <mkmehta2041@gmail.com>
2023-08-29 22:54:05 +0530

    added navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import Providers from "./Providers";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
           <Header />
 
           {/* Navbar  */}
+          <Navbar />
 
           {/* Search  */}
 
